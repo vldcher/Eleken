e4a062471e7d060dae2ed4d35f8819a772757d15
ChernenkoKM-21 <broobroo007@gmail.com>
2017-07-05 19:07:30 +0300

    add dropdown, minified imgs, add logo

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,29 @@
 $(function() {
 
-	// Custom JS
+	function DropDown(el) {
+		this.dd = el;
+		
+		this.initEvents();
+	}
+	DropDown.prototype = {
+		initEvents : function() {
+			var obj = this;
 
+			obj.dd.on('click', function(event){
+				$(this).toggleClass('active');
+				event.stopPropagation();
+			});	
+		}
+	}
+
+	$(function() {
+
+		var dd = new DropDown( $('#dd') );
+
+		$(document).click(function() {
+			// all dropdowns
+			$('.wrapper-dropdown-2').removeClass('active');
+		});
+
+	});
 });
